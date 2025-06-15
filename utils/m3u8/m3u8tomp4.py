be48ccd0be8ee3ba6dad426884c7e0f051f56702
cjc <49013706+cjc-github@users.noreply.github.com>
2025-06-15 20:09:25 +0800

    m3u8更新为类

--- utils/m3u8/m3u8tomp4.py
+++ utils/m3u8/m3u8tomp4.py
@@ -37,65 +37,71 @@ def remove_m3u8(base_path):
                 os.remove(index_m3u8)  # 删除文件
                 
                 
-def process_directory(base_path):
-    # 获取当前脚本的目录
-    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    count = 0
+class m3u8tomp4:
+    def __init__(self, path):
+        self.base_path = path
+                
+    def process_directory(self):
+        # 获取当前脚本的目录
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    log_entries = []
-    for entry in os.listdir(base_path):
-        entry_path = os.path.join(base_path, entry)
+        count = 0
 
-        # 检查是否为文件夹且以 .m3u8 结尾
-        if os.path.isdir(entry_path) and entry.endswith('.m3u8'):
-            # print("entry:", entry_path)
-            count += 1
+        log_entries = []
+        for entry in os.listdir(self.base_path):
+            entry_path = os.path.join(self.base_path, entry)
 
-            output_file = os.path.join(entry_path, 'output.mp4')
-            index_file = os.path.join(entry_path, 'index.m3u8')
-            video_dir = os.path.join(entry_path, 'video')
-            index = os.path.join(entry_path, 'index')
+            # 检查是否为文件夹且以 .m3u8 结尾
+            if os.path.isdir(entry_path) and entry.endswith('.m3u8'):
+                # print("entry:", entry_path)
+                count += 1
 
-            # 如果存在output.mp4文件
-            if os.path.exists(output_file):
-                log_entries.append(f"{entry}: 已存在")
+                output_file = os.path.join(entry_path, 'output.mp4')
+                index_file = os.path.join(entry_path, 'index.m3u8')
+                video_dir = os.path.join(entry_path, 'video')
+                index = os.path.join(entry_path, 'index')
 
-                continue
+                # 如果存在output.mp4文件
+                if os.path.exists(output_file):
+                    log_entries.append(f"{entry}: 已存在")
 
-            if os.path.exists(index_file):
-                # if not (os.path.exists(video_dir) and os.path.exists(index)):
-                # 存在video视频并不是没有下载完成
-                try:
-                    # 进入目标目录
-                    os.chdir(entry_path)
+                    continue
 
-                    # 下载完成，且未处理执行 ffmpeg 命令
-                    # ffmpeg_command = f"ffmpeg.exe -i index.m3u8 -c copy {output_file}"
-                    # ffmpeg_command = f"ffmpeg.exe -i index.m3u8 -c copy \"{output_file}\""
-                    ffmpeg_command = f"ffmpeg.exe -allowed_extensions ALL -i index.m3u8 -c copy \"{output_file}\""
-                    subprocess.run(ffmpeg_command, shell=True)
+                if os.path.exists(index_file):
+                    # if not (os.path.exists(video_dir) and os.path.exists(index)):
+                    # 存在video视频并不是没有下载完成
+                    try:
+                        # 进入目标目录
+                        os.chdir(entry_path)
 
-                    log_entries.append(f"{entry}: 转换成功")
-                except Exception as e:
-                    print("[!]", entry, "执行异常", e)
-            else:
-                log_entries.append(f"{entry}: 文件异常")
+                        # 下载完成，且未处理执行 ffmpeg 命令
+                        # ffmpeg_command = f"ffmpeg.exe -i index.m3u8 -c copy {output_file}"
+                        # ffmpeg_command = f"ffmpeg.exe -i index.m3u8 -c copy \"{output_file}\""
+                        ffmpeg_command = f"ffmpeg.exe -allowed_extensions ALL -i index.m3u8 -c copy \"{output_file}\""
+                        subprocess.run(ffmpeg_command, shell=True)
 
-    print("finish.", count)
+                        log_entries.append(f"{entry}: 转换成功")
+                    except Exception as e:
+                        print("[!]", entry, "执行异常", e)
+                else:
+                    log_entries.append(f"{entry}: 文件异常")
 
-    # 写入日志文件
-    timestamp = int(time.time())
-    log_file = os.path.join(script_dir, f"report_{timestamp}.log")  # 使用当前脚本目录
-    # log_file = f"report_{timestamp}.log"
-    try:
-        with open(log_file, 'w', encoding='utf-8') as f:
-            for log in log_entries:
-                f.write(log + '\n')
-    except Exception as e:
-        print(f"Failed to write log file: {e}")
+        print("finish.", count)
+
+        # 写入日志文件
+        timestamp = int(time.time())
+        log_file = os.path.join(script_dir, f"report_{timestamp}.log")  # 使用当前脚本目录
+        # log_file = f"report_{timestamp}.log"
+        try:
+            with open(log_file, 'w', encoding='utf-8') as f:
+                for log in log_entries:
+                    f.write(log + '\n')
+        except Exception as e:
+            print(f"Failed to write log file: {e}")
 
 
+# python m3u8tomp4.py E:\资源\视频
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <directory_path>")
@@ -103,6 +109,8 @@ if __name__ == "__main__":
 
     directory_path = sys.argv[1]
     # 转换
-    process_directory(directory_path)
+    task = m3u8tomp4(directory_path)
+    task.process_directory()
+    
     # 删除m3u8等文件
-    remove_m3u8(directory_path)
+    # remove_m3u8(directory_path)
